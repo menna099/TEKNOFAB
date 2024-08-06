f9e2a5a2e8254eba62d54e7581fa9a6adc94c0aa
mohashrafemt <102433465+menna099@users.noreply.github.com>
2024-08-06 10:39:41 +0300

    edit robot packing page,Checkweigher Conveyor page

--- src/Components/PackingMachines.jsx
+++ src/Components/PackingMachines.jsx
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 import Icons from "./Icons";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import weight from '../assets/weight.jpg'
 import IconOne from "../assets/icon_1.png";
 import IconTwo from "../assets/icon_2.png";
 import IconThree from "../assets/icon_3.png";
@@ -14,61 +15,61 @@ import IconFive from "../assets/icon_5.png";
 import IconSix from "../assets/icon_6.png";
 
 export default function PackingMachines({ language, setLanguage }) {
-  const [active, setActive] = useState(
-    "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0"
-  );
+  // const [active, setActive] = useState(
+  //   "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0"
+  // );
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
 
-  const images = [
-    {
-      original:
-        "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
-      thumbnail:
-        "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
-    },
-    {
-      original:
-        "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
-      thumbnail:
-        "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
-    },
-    {
-      original:
-        "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
-      thumbnail:
-        "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
-    },
-    {
-      original:
-        "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
-      thumbnail:
-        "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
-    },
-  ];
-  const photos = [
-    {
-      src: "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
-      width: 800,
-      height: 600,
-    },
-    {
-      src: "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
-      width: 1600,
-      height: 900,
-    },
-    {
-      src: "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
-      width: 1600,
-      height: 900,
-    },
-    {
-      src: "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
-      width: 1600,
-      height: 900,
-    },
-  ];
+  // const images = [
+  //   {
+  //     original:
+  //       "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
+  //     thumbnail:
+  //       "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
+  //   },
+  //   {
+  //     original:
+  //       "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
+  //     thumbnail:
+  //       "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
+  //   },
+  //   {
+  //     original:
+  //       "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
+  //     thumbnail:
+  //       "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
+  //   },
+  //   {
+  //     original:
+  //       "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
+  //     thumbnail:
+  //       "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
+  //   },
+  // ];
+  // const photos = [
+  //   {
+  //     src: "https://th.bing.com/th/id/R.61c3af0a54c0dc15821ac4f4896b39cc?rik=XNALvThEgGEBsQ&pid=ImgRaw&r=0",
+  //     width: 800,
+  //     height: 600,
+  //   },
+  //   {
+  //     src: "https://www.spackmachine.com/wp-content/uploads/2022/03/16-1.png",
+  //     width: 1600,
+  //     height: 900,
+  //   },
+  //   {
+  //     src: "https://th.bing.com/th/id/OIP.faZic5JW-Qp47nJQHUtacwAAAA?w=421&h=547&rs=1&pid=ImgDetMain",
+  //     width: 1600,
+  //     height: 900,
+  //   },
+  //   {
+  //     src: "https://th.bing.com/th/id/R.6b0a2b14d7d177c6826a771e1c58c43c?rik=M%2bXJrHOufYrsqQ&riu=http%3a%2f%2fwww.sinopham.com%2fcrab%2fimage%2forigin%2f2022%2f08%2f30%2f62a66a3d0fbb4d7ad65084e139227416.jpeg&ehk=5sQUFH44fWyzzjClPkn57TJFV7wPABEfHLbjX9yhioM%3d&risl=&pid=ImgRaw&r=0",
+  //     width: 1600,
+  //     height: 900,
+  //   },
+  // ];
 
   return (
     <Fragment>
@@ -79,12 +80,27 @@ export default function PackingMachines({ language, setLanguage }) {
             Checkweigher Conveyor
           </h1>
         </div>
-        {<RowsPhotoAlbum photos={photos} />}
-        <div className="checker-content">
-          <h1 className="text-SecondColor checker-content-heading mt-10 mb-5 text-center">
+       
+          <div
+            className="robot_about mb-10 flex items-center w-11/12 m-auto"
+            data-aos="zoom-in"
+          >
+            <div className="robot_desc flex-1" data-aos="zoom-in">
+              <h1 className="robot_about_heading text-2xl mb-2">
+                Automatic online check weigher with rejector suitable for different applications.
+              </h1>
+            </div>
+            <div className="about_robot_img text-center" data-aos="zoom-in">
+              <img src={weight} alt="Robot" className="w-96 hvr-pop rounded-lg" />
+            </div>
+          </div>
+        {/* {<RowsPhotoAlbum photos={photos} />} */}
+
+        <div className="checker-content" data-aos="zoom-in">
+          {/* <h1 className="text-SecondColor checker-content-heading mt-10 mb-5 text-center">
             Product Description
-          </h1>
-          <h4 className="font-bold text-lg text-center mb-5">
+          </h1> */}
+          <h4 className="font-bold text-xl text-center mb-10">
             key features and functions of an automatic checkweigher with
             rejector:
           </h4>
--- src/Components/RobotPacking.jsx
+++ src/Components/RobotPacking.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import AOS from "aos";
-import Robot_One from "../assets/pack_machine_1.webp";
+import video1 from "../assets/Video1.mp4";
+import video2 from "../assets/Video2.mp4";
+import video3 from "../assets/Video3.mp4";
+import packingrobot from "../assets/packingrobot.png";
+import { VscSend } from "react-icons/vsc";
 
 export default function RobotPacking({ language, setLanguage }) {
   useEffect(() => {
@@ -17,48 +21,68 @@ export default function RobotPacking({ language, setLanguage }) {
               {language === "AR"
                 ? ` الحلول `
                 : language === "EN"
-                ? `Packing Robot `
-                : "Çözümlerimiz"}
+                  ? `Packing Robot `
+                  : "Çözümlerimiz"}
             </h1>
           </div>
           <div
-            className="robot_about mb-10 flex gap-10 items-center w-11/12 m-auto"
+            className="robot_about mb-10 w-11/12 m-auto"
             data-aos="zoom-in"
           >
-            <div className="robot_desc flex-1" data-aos="zoom-in">
-              <h1 className="robot_about_heading font-extrabold text-xl">
-                World's Leading
+            <div className="" data-aos="zoom-in">
+              <h1 className="text-green-700 font-extrabold text-xl mb-5 text-left">
+                Custom-Robotic Solutions
               </h1>
-              <h1 className="robot_about_heading font-extrabold text-xl mb-4">
-                Robotic Palletizer Manufacturer
+
+              <ul className="ml-5">
+                <div className="flex items-center gap-2 mb-3"><VscSend /><li>Cost Effective & Space Saving</li></div>
+                <div className="flex items-center gap-2"><VscSend /><li>Applicable for different product shape</li></div>
+              </ul>
+            </div>
+            <div className="flex flex-col sm:flex-row items-center justify-center gap-3 mt-5">
+              <div className="mx-auto sm:w-1/2 w-full" data-aos="zoom-in">
+                <video controls loop>
+                  <source src={video1} type="video/mp4" />
+                </video>
+              </div>
+              <div className="mx-auto sm:w-1/2 w-full" data-aos="zoom-in">
+                <video controls loop>
+                  <source src={video2} type="video/mp4" />
+                </video>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div
+            className=" mb-10 flex flex-col sm:flex-row items-center justify-center w-11/12 m-auto"
+            data-aos="zoom-in"
+          >
+            <div className="" data-aos="zoom-in">
+              <h1 className="text-green-700 font-extrabold text-xl mb-5 text-left">
+                Delta Robot for Pick and Place
               </h1>
-              <p className="robot_desc_text leading-7">
-                Fuji Robotics is the Worlds Largest Robotic Palletizer
-                Manufacturer and has installed 17000 Robotic System. Fuji Yusoki
-                Kogyo Co., Ltd, Founded in April 13,1994 and Headquarters at
-                Sanyo-Onoda, Yamaguchi Prefecture, Japan. Fuji Robotics is the
-                Leading Manufacturer of Automatic & Industrial Robotic
-                Palletizer which is used for loading and offloading industrial
-                products such as bottles, cartons, boxes, etc. We are the World
-                Top Manufacturers of Robotic Palletizer with Energy Saving
-                Models such as Low-Medium Robotic Palletizer, High Speed Robotic
-                Palletizer, Heavy Payload Robotic Palletizer. Fuji Robotics
-                offers a fully automated Robotic System that ensures no human
-                contact during the Palletizing Operation. We emphasize a lot on
-                the safety of the Robotic palletizer by perfectly using
-                automation at all the possible pinch points in the system. The
-                smooth handling of the product and efficient stacking of the
-                boxes on pallet results in less product damages. While the
-                system works in high-speed, high-volume production applications,
-                it can make adjustments on the fly, switching seamlessly between
-                product types without the need to stop the line to change
-                programming.
-              </p>
+              <p className="mb-3 text-left">High Speed Case Packer</p>
+              <ul className="ml-5">
+                <div className="flex items-center gap-2 mb-3"><VscSend /><li>With Delta Robot up to <strong>120PPM</strong>
+                </li></div>
+                <div className="flex items-center gap-2 mb-3"><VscSend /><li>With Multi Robot can maximize the speed
+                </li></div>
+                <div className="flex items-center gap-2"><VscSend /><li>Optional for different bag styles/orientations
+                </li></div>
+              </ul>
+              <p className="mt-3 text-left">It is an excellent solution for your end-of-line automation</p>
+              <div className="sm:w-3/4 w-full mt-3" data-aos="zoom-in">
+                <video controls loop>
+                  <source src={video3} type="video/mp4" />
+                </video>
+              </div>
             </div>
             <div className="about_robot_img text-center" data-aos="zoom-in">
-              <img src={Robot_One} alt="Robot" className="mx-auto hvr-pop" />
+              <img src={packingrobot} alt="Robot" className="w-full sm:w-3/4 md:w-3/4 hvr-pop rounded-lg" />
             </div>
           </div>
+          {/*  */}
         </div>
       </div>
     </>
